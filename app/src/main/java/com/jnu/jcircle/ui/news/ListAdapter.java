530ff670772e0b3d72c2f39e8e2bce468fe5b54b
Tia <lyz15129392021@gmail.com>
2021-12-28 12:56:05 +0800

    fix

--- app/src/main/java/com/jnu/jcircle/ui/news/ListAdapter.java
+++ app/src/main/java/com/jnu/jcircle/ui/news/ListAdapter.java
@@ -38,13 +38,11 @@ public class ListAdapter extends BaseAdapter{
     public View getView(int position, View convertView, ViewGroup parent) {
         View view=mLayoutInflater.inflate(R.layout.list_item,null);
         announceList item=mList.get(position);
-        TextView uName=view.findViewById(R.id.uName);
         TextView title=view.findViewById(R.id.title);
         TextView details=view.findViewById(R.id.details);
         TextView date=view.findViewById(R.id.date);
         ImageView picture=view.findViewById(R.id.R_picture);
 
-        uName.setText(mList.get(position).getUName());
         title.setText(mList.get(position).getTitle());
         details.setText(mList.get(position).getDetails());
         date.setText(mList.get(position).getDate());
